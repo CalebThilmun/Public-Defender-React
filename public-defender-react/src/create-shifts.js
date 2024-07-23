9ff22aef9513b7ca4612837acf9611309ded1b20
CalebThilmun <caleb.thilmun@dimensiondata.com>
2024-07-23 12:16:16 +0200

    Shift creation functionality, with success modal

--- public-defender-react/src/create-shifts.js
+++ public-defender-react/src/create-shifts.js
@@ -5,11 +5,65 @@ import Sidebar from './sidebar';
 import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import Modal from 'react-modal';
 
 
 export default function ShiftCreation() {
 
     const [startDate, setStartDate] = useState(new Date());
+    const [hour, setHour] = useState("");
+    const [minute, setMinute] = useState("");
+    const [ampm, setAmpm] = useState("AM");
+    const [title, setTitle] = useState("");
+    const [shifts, setShifts] = useState([]);
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    
+    const ModalStyles = {
+        content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            marginRight: '-50%',
+            transform: 'translate(-50%, -50%)',
+            width: '30%',
+            border: '1px solid #ccc',
+            background: '#fff',
+            overflow: 'auto',
+            WebkitOverflowScrolling: 'touch',
+            borderRadius: '4px',
+            outline: 'none',
+            padding: '20px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center'
+            
+        }
+    }
+
+    //function called from creat shift button
+    const createShift = () => {
+        const newShift = {
+            date: startDate,
+            time: `${hour}:${minute} ${ampm}`,
+            title: title
+        };
+        localStorage.setItem('shifts', JSON.stringify([...shifts, newShift]));
+        
+        // Open the modal
+        setModalIsOpen(true);
+    };
+
+    const closeModal = () => {
+        // Close the modal and clear the fields
+        setModalIsOpen(false);
+        setStartDate(new Date());
+        setHour("");
+        setMinute("");
+        setAmpm("AM");
+        setTitle("");
+    };
 
   return (
     <div>
@@ -22,28 +76,38 @@ export default function ShiftCreation() {
       </div>
 
       <div id="time-picker">
-            <input type="number" id="hour" name="hour" min="1" max="12" placeholder="HH" />
-            <span id="time-picker-colon">:</span>
-            <input type="number" id="minute" name="minute" min="0" max="59" placeholder="MM" />
-            <select id="am-pm" name="am-pm">
-              <option value="AM">AM</option>
-              <option value="PM">PM</option>
-            </select>
-        </div>
+                <input type="number" id="hour" name="hour" min="1" max="12" placeholder="HH" onChange={(e) => setHour(e.target.value)} value={hour}/>
+                <span id="time-picker-colon">:</span>
+                <input type="number" id="minute" name="minute" min="0" max="59" placeholder="MM" onChange={(e) => setMinute(e.target.value)} value={minute}/>
+                <select id="am-pm" name="am-pm" onChange={(e) => setAmpm(e.target.value)} value={ampm}>
+                    <option value="AM">AM</option>
+                    <option value="PM">PM</option>
+                </select>
+            </div>
 
         <div id="shift-title-picker">
-        <select className="shift-title-picker">
-          <option value="">Please select a title for this shift...</option>
-          <option value="link1">Standard Weekday Day-Shift</option>
-          <option value="link2">Standard Weekday Night-Shift</option>
-          <option value="link3">Weekend Day-Shift</option>
-          <option value="link4">Weekend Night-shift</option>
-          <option value="link5">Special Over-Time Shift</option>
+        <select className="shift-title-picker" onChange={(e) => setTitle(e.target.value)} value={title}>
+                <option value="">Please select a title for this shift...</option>
+                <option value="Standard Weekday Day-Shift">Standard Weekday Day-Shift</option>
+                <option value="Standard Weekday Night-Shift">Standard Weekday Night-Shift</option>
+                <option value="Weekend Day-Shift">Weekend Day-Shift</option>
+                <option value="Weekend Night-shift">Weekend Night-shift</option>
+                <option value="Special Over-Time Shift">Special Over-Time Shift</option>
         </select>
 
         </div>
-          <button className="new-shift-creation">+ Create Shift</button>
+          <button className="new-shift-creation" onClick={createShift}>+ Create Shift</button>
           <button className="cancel-shift-creation">Cancel</button>
+
+          <Modal
+                style={ModalStyles}
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                contentLabel="Shift Created"
+            >
+                <h2>The shift has been created successfully!</h2>
+                <button className="modal-button" onClick={closeModal}>OK</button>
+            </Modal>
     </div>
      
   );
